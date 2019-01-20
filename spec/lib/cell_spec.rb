c620e0992c8900320eed51ba04b4cd2bfa155e98
Maxim Dzhuliy <mak7.dj@gmail.com>
2019-01-20 16:10:43 +0200

    add specs for toggle

--- spec/lib/cell_spec.rb
+++ spec/lib/cell_spec.rb
@@ -38,4 +38,33 @@ describe Cell do
       expect(cell.alive_neighbour_count).to eq 1
     end
   end
+
+  describe 'toogle' do
+    let(:neighbour)         { described_class.new(world, 1, 0) }
+    let(:second_neighbour)  { described_class.new(world, 1, 0) }
+    let(:next_neighbour)    { described_class.new(world, 1, 3) }
+    let(:dead_neighbour)    { described_class.new(world, 1, 2, live: false) }
+
+    context 'one alive neighbour' do
+      before do
+        world.add(cell)
+        world.add(neighbour)
+        cell.toggle
+      end
+
+      it { expect(cell.live).to be false }
+    end
+
+    context 'three alive neighbours' do
+      before do
+        world.add(cell)
+        world.add(neighbour)
+        world.add(second_neighbour)
+        world.add(next_neighbour)
+        cell.toggle
+      end
+
+      it { expect(cell.live).to be false }
+    end
+  end
 end
